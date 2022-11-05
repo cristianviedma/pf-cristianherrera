0277c0bb79554d5891f720fc0792671cd4450a65
cristianherrera2012@gmail.com <cristianherrera2012@gmail.com>
2022-11-05 19:32:16 -0300

    Actualziacion del proyecto

---
+++ js/menu.js
@@ -0,0 +1,17 @@
+(function(){
+    const openButton = document.querySelector('.nav');
+    const menu = document.querySelector('.nav');
+    const closeMenu = document.querySelector('.nav__close');
+
+    openButton.addEventListener('click', ()=>{
+        menu.classList.add('nav__link--show');
+    });
+
+    closeMenu.addEventListener('click', ()=>{
+        menu.classList.remove('nav__link--show');
+    });
+
+    
+
+
+})();
